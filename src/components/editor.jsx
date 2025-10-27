3962790a6a44777dbea8c68bb6eab750e0fe643e
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2025-10-27 10:28:29 -0300

    Set full Editor component structure

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -30,6 +30,77 @@ function Editor() {
 
         <button type='submit'>Save</button>
       </section>
+
+      <section className="header-info">
+        <h2>Education</h2>
+
+        <div className="form-group">
+          <label htmlFor="school">School</label>
+          <input type="text" name='school' id='school' />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="school">Degree</label>
+          <input type="text" name='degree' id='degree' />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="city">City</label>
+          <input type="text" name='city' id='city' />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="country">Country</label>
+          <input type="text" name='country' id='country' />
+        </div>
+
+        <div className="form-group">
+          <div className="start-date">
+            <label htmlFor="start-date">Start Date</label>
+            <input type="date" name='start-date' id='start-date' />
+          </div>
+
+          <div className="end-date">
+            <label htmlFor="end-date">End Date</label>
+            <input type="date" name="end-date" id="end-date" />
+          </div>
+        </div>
+
+        <button type='submit'>Add</button>
+      </section>
+
+      <section className="header-info">
+        <h2>Professional Experience</h2>
+
+        <div className="form-group">
+          <label htmlFor="job-title">Job Title</label>
+          <input type="text" name="job-title" id="job-title" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="company">Company</label>
+          <input type="text" name="company" id="company" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea name="description" id="description"></textarea>
+        </div>
+
+        <div className="form-group">
+          <div className="start-date">
+            <label htmlFor="start-date">Start Date</label>
+            <input type="date" name='start-date' id='start-date' />
+          </div>
+
+          <div className="end-date">
+            <label htmlFor="end-date">End Date</label>
+            <input type="date" name="end-date" id="end-date" />
+          </div>
+        </div>
+
+        <button type='submit'>Add</button>
+      </section>
     </aside>
   )
 }
