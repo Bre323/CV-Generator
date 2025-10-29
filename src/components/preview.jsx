7231cf07f316b12bc0c6837bd89eaf75fe6b6ff1
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2025-10-29 14:25:56 -0300

    Define preview initial styling sketch

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -3,27 +3,27 @@ import '../styles/preview.css';
 function Preview() {
   return (
     <main>
-      <div className='curriculum' style={{color: 'black'}}>
+      <div className='curriculum'>
         <div className='personal-info'>
           <h1>Breno Pena Rocha</h1>
           <div className="info">
-            <span>brenopenarocha2@gmail.com</span>
-            <span>988104449</span>
-            <span>Rua Pastor Pastor, 233</span>
+            <p>Email: brenopenarocha2@gmail.com</p>
+            <p>Phone: 988104449</p>
+            <p>Address: Rua Pastor Pastor, 233</p>
           </div>
         </div>
 
         <div className="education-info">
           <h2>Education</h2>
           <ul>
-            <li>
+            <li className='education-item'>
               <h3>School of schools</h3>
               <p>Bachelor</p>
               <p>Belem, Brazil</p>
               <p>20-10-2025 to 20-10-2025</p>
             </li>
 
-            <li>
+            <li className='education-item'>
               <h3>School of schools</h3>
               <p>Bachelor</p>
               <p>Belem, Brazil</p>
@@ -35,7 +35,7 @@ function Preview() {
         <div className="experience-info">
           <h2>Professional Experience</h2>
           <ul>
-            <li>
+            <li className='experience-item'>
               <h3>School of schools</h3>
               <p>Software Engineer</p>
               <p>20-10-2025 to 20-10-2025</p>
@@ -46,7 +46,7 @@ function Preview() {
               </p>
             </li>
 
-            <li>
+            <li className='experience-item'>
               <h3>X</h3>
               <p>Ruby on Rails Engineer</p>
               <p>20-10-2025 to 20-10-2025</p>
