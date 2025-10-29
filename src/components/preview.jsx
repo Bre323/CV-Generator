655de9d31a1b4c54d5f524bc210ab5bd6e0998ea
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2025-10-29 11:54:45 -0300

    Define preview initial structure sketch

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -3,7 +3,62 @@ import '../styles/preview.css';
 function Preview() {
   return (
     <main>
-      <div className='curriculum'></div>
+      <div className='curriculum' style={{color: 'black'}}>
+        <div className='personal-info'>
+          <h1>Breno Pena Rocha</h1>
+          <div className="info">
+            <span>brenopenarocha2@gmail.com</span>
+            <span>988104449</span>
+            <span>Rua Pastor Pastor, 233</span>
+          </div>
+        </div>
+
+        <div className="education-info">
+          <h2>Education</h2>
+          <ul>
+            <li>
+              <h3>School of schools</h3>
+              <p>Bachelor</p>
+              <p>Belem, Brazil</p>
+              <p>20-10-2025 to 20-10-2025</p>
+            </li>
+
+            <li>
+              <h3>School of schools</h3>
+              <p>Bachelor</p>
+              <p>Belem, Brazil</p>
+              <p>20-10-2025 to 20-10-2025</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="experience-info">
+          <h2>Professional Experience</h2>
+          <ul>
+            <li>
+              <h3>School of schools</h3>
+              <p>Software Engineer</p>
+              <p>20-10-2025 to 20-10-2025</p>
+              <p>
+                ofsnagopnspbnorbnwoiarwnbaoeineobenberoibnornbroinonboi
+                eRNBAPIORNBIONOIERNOINOINonoiwbnoieprbnopibenpoernsbopin
+                aeorinbpoeirbnoirnbopbnerobnoinorinboirnboinboiernbooirba
+              </p>
+            </li>
+
+            <li>
+              <h3>X</h3>
+              <p>Ruby on Rails Engineer</p>
+              <p>20-10-2025 to 20-10-2025</p>
+              <p>
+                ofsnagopnspbnorbnwoiarwnbaoeineobenberoibnornbroinonboi
+                eRNBAPIORNBIONOIERNOINOINonoiwbnoieprbnopibenpoernsbopin
+                aeorinbpoeirbnoirnbopbnerobnoinorinboirnboinboiernbooirba
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
     </main>
   )
 }
